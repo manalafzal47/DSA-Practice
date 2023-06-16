57f02dd362addc2a16fbfa1e1f4cb422fcd7a464
Manal Afzal <manal.afzal@ontariotechu.net>
2023-06-16 17:40:20 -0400

    Update Duplicate-Zero.py

--- Arrays/Duplicate-Zero.py
+++ Arrays/Duplicate-Zero.py
@@ -2,9 +2,9 @@
 # shifting the remaining elements to the right.
 
 def duplicateZeros(self, original_arr):
-    # move the elements from the array to the new array
+    # move the elements from the original array to the new array
     new_arr = original_arr[:]
-    # clear the array 
+    # clear the original array 
     original_arr[:] = []
     
     # traverse through the array, and find where the array element is a 0
@@ -13,11 +13,9 @@ def duplicateZeros(self, original_arr):
         if new_arr[i] == 0:
             original_arr.append(0)
         
-        # if it is another element other than 0, then 
+        # if it is another element other than 0, then append it to the end of the list
         original_arr.append(new_arr[i])
     
-    # delete the length of the original array 
+    # deleting the original array length to account for memory 
     del original_arr[len(new_arr):]
-
-
 
